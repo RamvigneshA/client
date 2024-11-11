bcfc8d0335d95536f28a8f78c44ae86d8a3b1861
RamvigneshA <ramvigneshnanban@gmail.com>
2024-11-11 11:58:07 +0530

    error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -9,25 +9,23 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import Add from './pages/Add';
-import Books from './pages/Books';
-import Update from './pages/Update';
+import Add from './pages/Add.jsx';
+import Books from './pages/Books.jsx';
+import Update from './pages/Update.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Navigate to="/books" />} />
-      <Route  path='books' element={<Books />} />
+      <Route path="books" element={<Books />} />
       <Route path="/add" element={<Add />} />
       <Route path="/update/:id" element={<Update />} />
     </Route>
   )
 );
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-   
-  </StrictMode>,
-)
+  </StrictMode>
+);
